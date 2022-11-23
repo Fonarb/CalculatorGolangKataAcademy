3c0cf3c6cd4a6d875ddfe1ff27d52f5bff2584db
pavel_shein <shellgod7@gmail.com>
2022-11-23 09:26:42 +0400

    1.1

--- main.go
+++ main.go
@@ -117,16 +117,26 @@ func intToRome(i int) string {
 }
 
 var num = map[string]int{
-	"I":    1,
-	"II":   2,
-	"III":  3,
-	"IV":   4,
-	"V":    5,
-	"VI":   6,
-	"VII":  7,
-	"VIII": 8,
-	"IX":   9,
-	"X":    10,
+	"I":     1,
+	"II":    2,
+	"III":   3,
+	"IV":    4,
+	"V":     5,
+	"VI":    6,
+	"VII":   7,
+	"VIII":  8,
+	"IX":    9,
+	"X":     10,
+	"XI":    11,
+	"XII":   12,
+	"XIII":  13,
+	"XIV":   14,
+	"XV":    15,
+	"XVI":   16,
+	"XVII":  17,
+	"XVIII": 18,
+	"XIX":   19,
+	"XX":    20,
 }
 
 func RegSplit(text string, delimeter string) []string {
@@ -147,17 +157,4 @@ func RegSplit(text string, delimeter string) []string {
 	fmt.Println("Неверный знак")
 
 	return a
-}
-func isWrongOperation(s string) bool {
-	switch s {
-	case "*":
-		return false
-	case "-":
-		return false
-	case "/":
-		return false
-	case "+":
-		return false
-	}
-	return true
 }
